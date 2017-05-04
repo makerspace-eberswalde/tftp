922dae7896dcbc38896d1248c3bc06a20ac49e0b
divramod <divramod@gmail.de>
2017-05-04 09:30:49 +0200

    start level problem with dnsmasq

--- install/install.sh
+++ install/install.sh
@@ -8,12 +8,12 @@ echo "==========================================================================
 echo "\nSTART dnsmasq INSTALLATION"
 echo "--------------------------------------------------------------------------------"
 # TODO: install dnsmasq
-sudo apt-get install -y dnsmasq &> /dev/null &&
+sudo apt-get install -y dnsmasq &&
 
 echo "\nSTART apache2 INSTALLATION"
 echo "--------------------------------------------------------------------------------"
 # TODO: install apache2/http-server
-sudo apt-get install -y appache2 &&
+sudo apt-get install -y apache2 &&
 
 # [MESSAGE]
 echo "\nDONE TFTP-INSTALLATION"
