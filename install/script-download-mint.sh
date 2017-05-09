fb83346a87ae10abb0a3dbb1017609db35379ae7
Arvid Petermann <divramod@gmail.com>
2017-05-09 04:09:51 +0200

    add download-mint

--- install/script-download-mint.sh
+++ install/script-download-mint.sh
@@ -1,2 +1,4 @@
-wget http://ftp.fau.de/mint/iso/linuxmint.com/stable/18.1/linuxmint-18.1-mate-32bit.iso
-
+sudo mkdir -p /var/lib/tftpboot/distros/mint &&
+	cd /var/lib/tftpboot/distros/mint &&
+	sudo wget http://ftp.fau.de/mint/iso/linuxmint.com/stable/18.1/linuxmint-18.1-mate-32bit.iso &&
+	sudo cd -
