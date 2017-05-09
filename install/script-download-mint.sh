5eb92813c35570822fa8f7be69960132e293ecf1
Arvid Petermann <divramod@gmail.com>
2017-05-09 04:48:09 +0200

    update

--- install/script-download-mint.sh
+++ install/script-download-mint.sh
@@ -1,31 +1,41 @@
+#!/bin/sh	
+getOsMint() {
+	DIR=$1
+	FILE=$1.iso
+	FILEPATH=$2
+	# [MESSAGE]
+	echo "\nSTART $FILE DOWNLOAD"
+	echo "--------------------------------------------------------------------------------"
+	if [ ! -f "$FILE" ]; then
+		sudo wget $FILEPATH
+	fi
+	#sudo rm -rf "$DIR" &&
+		sudo mkdir $DIR
+	#sudo 7z x $FILE -o $DIR
+	echo "\nDONE $FILE DOWNLOAD"
+	echo "--------------------------------------------------------------------------------"
+}
+
+testf() {
+	echo "in"
+	sudo rm -rf /tmp/test
+}
+
 # [MESSAGE]
 echo "\nSTART DOWNLOAD MINT"
 echo "================================================================================"
 
-getOs() {
-  FILE=$1
-  FILEPATH=$2
-  # [MESSAGE]
-  echo "\nSTART $FILE DOWNLOAD"
-  echo "--------------------------------------------------------------------------------"
-  if [ ! -d "$FILE" ]; then
-    sudo wget $FILEPATH &&
-      sudo 7z x $FILEPATH
-  fi
-  echo "\nDONE $FILE DOWNLOAD"
-  echo "--------------------------------------------------------------------------------"
-}
-
 # [RUN]
 sudo mkdir -p /var/lib/tftpboot/distros/mint &&
-  cd /var/lib/tftpboot/distros/mint &&
+	cd /var/lib/tftpboot/distros/mint &&
 
-  FILENAME=linuxmint-18.1-mate-32bit.iso &&
-  PATH=http://ftp.fau.de/mint/iso/linuxmint.com/stable/18.1/$FILENAME &&
-  getOs $FILENAME $PATH
+	VERSION=linuxmint-18.1-mate-32bit &&
+	PATH=http://ftp.fau.de/mint/iso/linuxmint.com/stable/18.1/$VERSION.iso &&
+	getOsMint $VERSION $PATH &&
 
-  sudo cd -
+	cd -
 
-  # [MESSAGE]
-  echo "\nDONE DOWNLOAD MINT"
-  echo "================================================================================"
+	# [MESSAGE]
+	echo "\nDONE DOWNLOAD MINT"
+	echo "================================================================================"
+
