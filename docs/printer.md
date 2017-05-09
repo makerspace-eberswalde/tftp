28988fc1fd3556b509bfff37eb2e06fd6394f935
Arvid Petermann <divramod@gmail.com>
2017-05-09 02:26:16 +0200

    add docs/printer.md

---
+++ docs/printer.md
@@ -0,0 +1,4 @@
+* install cups: https://wiki.ubuntuusers.de/CUPS/
+** sudo apt-get install cups cups-client cups-bsd
+* share printer:
+** sudo cupsctl --share-printers
--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,7 @@
 sh /var/lib/tftpboot/install/script-install-software.sh &&
-  sh /var/lib/tftpboot/install/configure.sh &&
-  sh /var/lib/tftpboot/install/script-restart-services.sh &&
-  sh /var/lib/tftpboot/install/script-download-distros.sh
+	sh /var/lib/tftpboot/install/configure.sh &&
+	sh /var/lib/tftpboot/install/script-services-restart.sh &&
+	sh /var/lib/tftpboot/install/script-download-distros.sh
 
 # [MESSAGE]
 echo "\n\n For convienience install github.com/makerspace-eberswalde/dotfiles cli tools. You can than use the zsh aliases to start, stop, remove, conigure the tftp server!"
