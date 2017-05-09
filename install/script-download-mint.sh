0c970484709b2a60e89789b7253cfe229336e19a
divramod <divramod@gmail.de>
2017-05-09 04:18:18 +0200

    update mint download script

--- install/script-download-mint.sh
+++ install/script-download-mint.sh
@@ -1,5 +1,31 @@
+# [MESSAGE]
+echo "\nSTART DOWNLOAD MINT"
+echo "================================================================================"
+
+getOs() {
+  FILE=$1
+  FILEPATH=$2
+  # [MESSAGE]
+  echo "\nSTART $FILE DOWNLOAD"
+  echo "--------------------------------------------------------------------------------"
+  if [ ! -d "$FILE" ]; then
+    sudo wget $FILEPATH &&
+      sudo 7z x $FILEPATH
+  fi
+  echo "\nDONE $FILE DOWNLOAD"
+  echo "--------------------------------------------------------------------------------"
+}
+
+# [RUN]
 sudo mkdir -p /var/lib/tftpboot/distros/mint &&
-	cd /var/lib/tftpboot/distros/mint &&
-	sudo wget http://ftp.fau.de/mint/iso/linuxmint.com/stable/18.1/linuxmint-18.1-mate-32bit.iso &&
-  sudo 7z x linuxmint-18.1-mate-32bit.iso &&
-	sudo cd -
+  cd /var/lib/tftpboot/distros/mint &&
+
+  FILENAME=linuxmint-18.1-mate-32bit.iso &&
+  PATH=http://ftp.fau.de/mint/iso/linuxmint.com/stable/18.1/$FILENAME &&
+  getOs $FILENAME $PATH
+
+  sudo cd -
+
+  # [MESSAGE]
+  echo "\nDONE DOWNLOAD MINT"
+  echo "================================================================================"
